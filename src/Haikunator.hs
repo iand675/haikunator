f1f07e7e79414ad5fbf49dde20383956ec93dc82
rshaked <rotem@shaked.us>
2022-08-04 15:54:08 -0700

    hlint

--- src/Haikunator.hs
+++ src/Haikunator.hs
@@ -50,7 +50,7 @@ haikunate :: RandomGen g => HaikuGeneratorConfig -> g -> (HaikuSlug, g)
 haikunate conf g =
   let (adjIx, g') = uniformR (0, length (adjectives conf) - 1) g
       (nounIx, g'') = uniformR (0, length (nouns conf) - 1) g'
-      (number, g''') = uniformR (0, 10 ^ (numberOfDigits conf) - 1) g''
+      (number, g''') = uniformR (0, 10 ^ numberOfDigits conf - 1) g''
   in 
     ( HaikuSlug 
         (adjectives conf V.! adjIx) 
